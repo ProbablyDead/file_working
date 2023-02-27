d025583aa22c67a84e5b5ab29938e0c56bb273a7
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2023-02-27 03:29:50 +0300

    finish

---
+++ FileWorker.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+import  java.io.FileOutputStream;
+
+public class FileWorker {
+  private HashMap<Integer, Integer> table = new HashMap<>();
+  private String inputPath;
+  private String outputPath;
+
+  FileWorker () {
+    Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter input file's path\n-> ");
+    this.inputPath = in.nextLine();
+    System.out.print("Enter output file's path\n-> ");
+    this.outputPath = in.nextLine();
+
+    in.close();
+  }
+
+  FileWorker (String inputPath, String outputPath) {
+    this.inputPath = inputPath;
+    this.outputPath = outputPath;
+  }
+
+  private void addValue (int c) {
+    if (table.containsKey(c)) {
+      int count = table.get(c);
+      table.put(c, ++count);
+    }
+    else {
+      table.put(c, 1);
+    }
+  }
+
+  private void updateMapWtString (String str) {
+    str.chars().forEach(c -> addValue(c));
+  }
+
+  private String getResultString () {
+    String result = "";
+
+    for (int a : table.keySet()) {
+        result += (char)a + " - " + table.get(a) + "\n"; 
+    }
+    
+    return result;
+  }
+
+  public void checkFile () {
+    BufferedReader inputBuf;
+    FileOutputStream outputStream;
+
+      try {
+        inputBuf = new BufferedReader(new FileReader(new File(inputPath)));
+      }
+      catch (IOException e) {
+        System.out.println("Input file's path is invalid");
+        return;
+      }
+
+      inputBuf.lines().forEach(str -> updateMapWtString(str));
+      try {
+        outputStream = new FileOutputStream(outputPath);
+        outputStream.write(getResultString().getBytes());
+        outputStream.close();
+      }
+      catch (IOException e) {
+        System.out.println("Output file's path is invalid");
+      }
+  }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+  public static void main (String[] args) {
+    FileWorker a = new FileWorker();
+    a.checkFile();
+  }
+}
